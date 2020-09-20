cc21bb9cd8f1b6d229c5719ab68afa58b382a968
Nikita Zhiga <assisken@gmail.com>
2020-09-21 02:19:24 +0300

    Initial commit

---
+++ first/01test/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+exec 0<>/dev/console 1<>/dev/console 2<>/dev/console
+
+cat <<'msgend'
+ ___________________
+< Let's kill da ho! >
+ -------------------
+   \
+    \    ___
+        /   \
+  |\   |o_o  |
+  \ \  |:_/  |
+   \_\//    \ \
+     (|      | )
+     /'\_   _/'\
+     \___)=(___/
+msgend
+sleep 10
